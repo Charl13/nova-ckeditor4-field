4975ab81765bb170f700c89da642f5370ea1c06a
Charlie <charlie.vieillard@pixelindustries.com>
2018-12-07 12:26:16 +0100

    Added locales to initial field metadata. Added methods resolveAttribute() and fillAttributeFromRequest() to extract and hydrate values based on locales.

--- src/NovaCkeditor.php
+++ src/NovaCkeditor.php
@@ -4,6 +4,7 @@ namespace Waynestate\Nova;
 
 use Laravel\Nova\Fields\Field;
 use Laravel\Nova\Fields\Expandable;
+use Laravel\Nova\Http\Requests\NovaRequest;
 
 class CKEditor extends Field
 {
@@ -20,9 +21,54 @@ class CKEditor extends Field
     {
         parent::__construct($name, $attribute, $resolveCallback);
 
+        $locales = array_map(function ($value) {
+            return __($value);
+        }, config('translatable.locales'));
+
         $this->withMeta([
-            'options' => config('nova.ckeditor-field.options', [])
+            'options' => config('nova.ckeditor-field.options', []),
+            'locales' => $locales,
+            'indexLocale' => app()->getLocale(),
         ]);
+    }
+
+    /**
+     * Resolve the given attribute from the given resource.
+     *
+     * @param  mixed  $resource
+     * @param  string  $attribute
+     * @return mixed
+     */
+    protected function resolveAttribute($resource, $attribute)
+    {
+        $results = [];
+
+        $translations = $resource->translations()
+            ->get(['locale', $attribute])
+            ->toArray();
+        foreach ( $translations as $translation ) {
+            $results[$translation['locale']] = $translation[$attribute];
+        }
+
+        return $results;
+    }
+
+    /**
+     * Hydrate the given attribute on the model based on the incoming request.
+     *
+     * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
+     * @param  string  $requestAttribute
+     * @param  object  $model
+     * @param  string  $attribute
+     * @return void
+     */
+    protected function fillAttributeFromRequest(NovaRequest $request, $requestAttribute, $model, $attribute)
+    {
+        if ( is_array($request[$requestAttribute]) ) {
+            foreach ( $request[$requestAttribute] as $lang => $value ) {
+                $model->translateOrNew($lang)->{$attribute} = $value;
+            }
+        }
     }
 
     /**
@@ -51,5 +97,16 @@ class CKEditor extends Field
         return array_merge(parent::jsonSerialize(), [
             'shouldShow' => $this->shouldBeExpanded(),
         ]);
+    }
+
+    /**
+     * Set the locales to display / edit.
+     *
+     * @param  array  $locales
+     * @return $this
+     */
+    public function locales(array $locales)
+    {
+        return $this->withMeta(['locales' => $locales]);
     }
 }
